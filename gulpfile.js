f732afd8d7d5622351ccb56c5052d0a99ebdecd5
Avi Dunn <avidunn@gmail.com>
2014-12-18 10:00:55 -0800

    Added test server file, added .travis.yml file, modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,31 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
+var jsPaths = {
+  path: ['server/**/*.js']
+};
+
 // checks js files with jshint
 gulp.task('lint', function() {
-  return gulp.src('src/**/*.js')
+  // return gulp.src('server/**/*.js')
+  return gulp.src(jsPaths.path) 
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 // checks js files for jscs styles
 gulp.task('checkStyle', function() {
-  return gulp.src('src/**/*.js')
+  // return gulp.src('./**/*.js')
+  return gulp.src(jsPaths.path)
     .pipe(jscs({
       preset: 'airbnb'
     }));
 });
 
 // watch files for changes
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', ['lint', 'checkStyle']);
-});
+// gulp.task('watch', function() {
+//   gulp.watch('src/**/*.js', ['lint', 'checkStyle']);
+// });
 
-gulp.task('default', ['lint', 'checkStyle', 'watch']);
+gulp.task('default', ['lint', 'checkStyle']);
+
