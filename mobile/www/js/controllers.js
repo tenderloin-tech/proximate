7124e18b489eb3eec7b12ef45ae699a5743b37d1
Valentyn Boginskey <poison5151@gmail.com>
2014-12-22 08:53:39 -0500

    Removed generated templates, added storage factory

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -1,27 +1,8 @@
-angular.module('starter.controllers', [])
+angular.module('proximate.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('StatusCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope, Beacons, PubNub) {
+.controller('SettingsCtrl', function($scope, Beacons, PubNub) {
   $scope.settings = {
     enableFriends: true
   };
