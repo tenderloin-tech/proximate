e4b9b7432f236412470791ae9b3bd12f0a9ea2bd
Derek Barncard <derek@barncard.com>
2014-12-22 15:10:20 -0800

    Added POST on username change and stemmed server route. Also added server settings to mobile config.js

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -6,20 +6,17 @@ angular.module('proximate.controllers', [])
 
 .controller('SettingsCtrl', function($scope, Beacons, PubNub, Settings) {
 
-    Settings.updateDeviceId();
 
+    //waits for the view to load so as to not interrupt the html/css
     angular.element(document).ready(function() {
 
         Beacons.setupTestBeacons(PubNub.publish);
 
         $scope.data = Settings.data;
 
-        setTimeout(function(){
-          Settings.updateDeviceId();
-          $scope.deviceId = Settings.deviceId;
-        },1000);
-
     });
+
+    /* scope functions */
 
     $scope.updateUsername = function(){
         Settings.updateUsername($scope.data.username);
