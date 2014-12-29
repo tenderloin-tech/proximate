0c369218beb8833ebb083918beb67f011bd38870
Sebastian Tonkin <sgtonkin@gmail.com>
2014-12-29 15:41:53 -0800

    Created new server routes and tied to DB helper functions

--- server/routes.js
+++ server/routes.js
@@ -8,7 +8,8 @@ module.exports = function(app) {
   // Register a deviceId to a participant
   // Expect body of the request to contain:
   // {deviceId: 'B19A9282-3124-4A3D-A387-60B4E92F22AF', username: 'Meat puppet'}
-  app.post('/api/register', function(req, res) {
+  app.post('/api/devices/register', function(req, res) {
+
     new models.Participant({
         name: req.body.username,
         deviceId: req.body.deviceId
@@ -19,15 +20,16 @@ module.exports = function(app) {
       }, function() {
         res.status(404).send('Invalid username');
       });
+
   });
 
   // Return a list of beacons for a participant
   // Expect body of the request to contain:
   // {username: 'Meat puppet'}
   // This should fetch from the DB in the future
-  app.post('/api/beacons', function(req, res) {
-    console.log('%s requested beacons', req.body.username);
+  app.get('/api/beacons/:deviceId', function(req, res) {
 
+    var deviceId = req.params.deviceId;
     var testRegions = [{
         uuid : 'E2C56DB5-DFFB-48D2-B060-D0F5A71096E0',
         identifier : 'Apple AirLocate E2C56DB5',
@@ -37,14 +39,16 @@ module.exports = function(app) {
 
     console.log('Sending: ' + testRegions);
     res.json(testRegions);
+
   });
 
   // Return a list of events for a participant
-  // Expect body of the request to contain:
-  // {username: 'Meat puppet'}
-  app.post('/api/events', function(req, res) {
+  app.get('/api/events/:participantId', function(req, res) {
+
+    var participantId = req.params.participantId;
+
     new models.Participant({
-      name: req.body.username
+      id: participantId
     })
       .fetch({withRelated: ['events'], require: true})
       .then(function(participant) {
@@ -52,20 +56,66 @@ module.exports = function(app) {
       }, function() {
         res.status(404).send('Invalid username');
       });
+
   });
 
   // Get event participants for a given eventId
-  app.get('/api/participants/:eventId', function(req, res) {
+  app.get('/api/events/participants/:eventId', function(req, res) {
 
     var eventId = req.params.eventId;
 
-     helpers.getEventParticipants(eventId)
+    helpers.getEventParticipants(eventId)
     .then(function(model) {
       res.json(model.toJSON());
     })
     .catch(function(error) {
-      res.send("Invalid event ID");
+      res.send('Invalid event ID');
     });
 
   });
+
+  // Get info for the most current event from the db
+  app.get('/api/events/current', function(req, res) {
+
+    helpers.getCurrentEvent()
+      .then(function(model) {
+        res.json(model.toJSON());
+      })
+      .catch(function(error) {
+        res.status(404).send('Unable to fetch current event data');
+      });
+
+  });
+
+  // Get the participant info for a given device ID
+  app.get('/api/participants/devices/:deviceId', function(req, res) {
+
+    var deviceId = req.params.deviceId;
+
+    helpers.getParticipant(deviceId)
+      .then(function(model) {
+        res.json(model.toJSON());
+      })
+      .catch(function(error) {
+        res.status(404).send('Unable to fetch participant info');
+      })
+
+  });
+
+  // Get the checkin status for a given device and event
+  app.get('/api/participants/status/:deviceId/:eventId', function(req, res) {
+
+    var deviceId = req.params.deviceId;
+    var eventId = req.params.eventId;
+
+    helpers.getCheckinStatus('999','1')
+      .then(function(model) {
+        res.json(model.toJSON());
+      })
+      .catch(function(error) {
+        res.status(404).send('Unable to fetch checkin status');
+      });
+
+  });
+
 };
