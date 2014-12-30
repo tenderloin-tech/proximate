f966f80b7991ea98686d51f49e069da3caa0ca58
Derek Barncard <derek@barncard.com>
2014-12-29 21:31:53 -0800

    Separated factories into individual files

---
+++ mobile/www/js/services/localstorageFactory.js
@@ -0,0 +1,20 @@
+angular.module('proximate.services', [])
+
+// Storage factory, uses window.localStorage
+// Includes methods for storing objects
+.factory('$localStorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  };
+}]);
