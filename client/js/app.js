4b36e078de22e88e189a57d6700f77b63741a893
Avi Dunn <avidunn@gmail.com>
2014-12-30 10:13:59 -0800

    Removed run function in app.js and added functionality to event ctrl

--- client/js/app.js
+++ client/js/app.js
@@ -1,25 +1,9 @@
 angular.module('proximate',
   ['ui.router',
   'proximate.controllers',
-  'proximate.services'
-  // 'pubnub.angular.service'
+  'proximate.services',
+  'angularMoment'
   ])
-
-.run(function(PubNub) {
-  // PubNub.subscribe('checkins');
-  // console.log('pubnub in client app', PubNub);
-  // console.log('publish in run', PubNub.publish);
-  // console.log('subscribe in run', PubNub.subscribe);
-  // setTimeout(function(){
-  //   PubNub.publish('checkins', {
-  //     deviceId: 'deviceId',
-  //     username: 'username',
-  //     region: 'E2C56DB5-DFFB-48D2-B060-D0F5A71096E0',
-  //     eventType: 'enterRegion'
-  //   });
-  // }, 10000);
-  // change to just invoking function to grab list of attendees
-})
 
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,29 +1,31 @@
 angular.module('proximate.controllers', [])
 
 .controller('AdminCtrl', function($scope, Populate, PubNub) {
-  $scope.participants = {};
-  $scope.currentEvent = {};
-
   angular.extend($scope, Populate);
+
+  $scope.newCheckins = [];
 
   $scope.getCurrentEvent(function(eventData) {
     $scope.eventData = eventData;
-    console.log('eventData', eventData);
+    // console.log('eventData', eventData);
     $scope.getParticipants(eventData.data.id, function(participantData) {
-      $scope.participantData = participantData;
-      console.log($scope.participantData);
+      $scope.participantData = participantData.data[0].participants;
+      // console.log($scope.participantData);
     });
   });
 
   PubNub.subscribe('checkins', function(message) {
-    console.log(message);
-    // push to array
+    // console.log(message);
+    $scope.newCheckins.push(message.username);
     // use array in custom filter
   });
 
 })
 
-.controller('EventCtrl', function($scope) {
+.controller('EventCtrl', function($scope, Populate, PubNub) {
+
+  $scope.newCheckins = [];
+
   var updateClock = function() {
     $scope.clock = new Date();
   };
@@ -32,4 +34,22 @@ angular.module('proximate.controllers', [])
     $scope.$apply(updateClock);
   }, 1000);
   updateClock();
+
+  angular.extend($scope, Populate);
+
+  $scope.getCurrentEvent(function(eventData) {
+    $scope.eventData = eventData;
+    // console.log('eventData', eventData);
+    $scope.getParticipants(eventData.data.id, function(participantData) {
+      $scope.participantData = participantData.data[0].participants;
+      // console.log($scope.participantData);
+    });
+  });
+
+  PubNub.subscribe('checkins', function(message) {
+    // console.log(message);
+    $scope.newCheckins.push(message.username);
+    // use array in custom filter
+  });
+
 });
