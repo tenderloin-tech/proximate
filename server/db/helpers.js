9ee83502d77f01b3fa2b1e5e431054db6a3bc7ac
Valentyn Boginskey <poison5151@gmail.com>
2014-12-30 16:22:50 -0800

    Reverted breaking refactor of models.js and helpers.js

--- server/db/helpers.js
+++ server/db/helpers.js
@@ -2,149 +2,156 @@ var models = require('../models');
 var moment = require('moment');
 
 // POST HELPERS
-module.exports = {
 
-  updateDeviceId: function(username, deviceId) {
+var updateDeviceId = function(username, deviceId) {
 
-    return new models.Participant()
-      .query({where: {name: username}})
-      .fetch()
-      .then(function(model) {
-        model.set('device_id', deviceId);
-        model.save();
-        return model;
-      });
+  return new models.Participant()
+    .query({where: {name: username}})
+    .fetch()
+    .then(function(model) {
+      model.set('device_id', deviceId);
+      model.save();
+      return model;
+    });
 
-  },
+};
 
-  // GET HELPERS
+// GET HELPERS
 
-  getEvents: function(participantId) {
+var getEvents = function(participantId) {
 
-    return new models.Participant()
-      .query({where: {id: participantId}})
-      .fetch({withRelated: ['events'], require: true})
-      .then(function(model) {
-        return model;
-      });
+  return new models.Participant()
+    .query({where: {id: participantId}})
+    .fetch({withRelated: ['events'], require: true})
+    .then(function(model) {
+      return model;
+    });
 
-  },
+};
 
-  getEventParticipants: function(eventId) {
+var getEventParticipants = function(eventId) {
 
-    return new models.Events()
-      .query({where:{id: eventId}})
-      .fetch({withRelated: ['participants'], require: true})
-      .then(function(collection) {
-        return collection;
-      });
+  return new models.Events()
+    .query({where:{id: eventId}})
+    .fetch({withRelated: ['participants'], require: true})
+    .then(function(collection) {
+      return collection;
+    });
 
-  },
+};
 
-  getParticipant: function(deviceId) {
+var getParticipant = function(deviceId) {
 
-    return new models.Participant()
-      .query({where: {device_id: deviceId}})
-      .fetch({require: true})
-      .then(function(model) {
-        return model;
-      });
+  return new models.Participant()
+    .query({where: {device_id: deviceId}})
+    .fetch({require: true})
+    .then(function(model) {
+      return model;
+    });
 
-  },
+};
 
-  getCheckinStatus: function(deviceId, eventId) {
+var getCheckinStatus = function(deviceId, eventId) {
 
-    var participant_id;
+  var participant_id;
 
-    console.log('deviceId', deviceId, 'eventId', eventId);
+  console.log('deviceId', deviceId, 'eventId', eventId);
 
-    return getParticipant(deviceId)
-      .then(function(model) {
-        participant_id = model.get('id');
-      })
-      .then(function() {
-        return new models.EventsParticipants()
-          .query({where:{participant_id: participant_id, event_id: eventId}})
-          .fetch({require: true})
-          .then(function(model) {
-            return model;
-          });
-      });
+  return getParticipant(deviceId)
+    .then(function(model) {
+      participant_id = model.get('id');
+    })
+    .then(function() {
+      return new models.EventsParticipants()
+        .query({where:{participant_id: participant_id, event_id: eventId}})
+        .fetch({require: true})
+        .then(function(model) {
+          return model;
+        });
+    });
 
-  },
+};
 
-  getCurrentEvent: function() {
+var getCurrentEvent = function() {
 
-    return new models.Event()
-      .query('orderByRaw', 'ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(start_time)) ASC')
-      .fetch({require:true})
-      .then(function(model) {
-        return model;
-      });
+  return new models.Event()
+    .query('orderByRaw', 'ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(start_time)) ASC')
+    .fetch({require:true})
+    .then(function(model) {
+      return model;
+    });
 
-  },
+};
 
-  // PUBNUB HELPERS
+// PUBNUB HELPERS
 
-  checkinUser: function(deviceId, callback) {
+var checkinUser = function(deviceId, callback) {
 
-    var participantId;
-    var eventId;
-    var eventStartTime;
-    var status;
-    var now = moment();
+  var participantId;
+  var eventId;
+  var eventStartTime;
+  var status;
+  var now = moment();
 
-    // Get the participant_id from the deviceID
-    var user = new models.Participant({device_id: deviceId})
-      .fetch({require:true})
-      .then(function(model) {
-        participantId = model.get('id');
-      })
+  // Get the participant_id from the deviceID
+  var user = new models.Participant({device_id: deviceId})
+    .fetch({require:true})
+    .then(function(model) {
+      participantId = model.get('id');
+    })
 
-      // Get the event_id of the closest event in time
-      .then(function() {
-        new models.Event()
-          .query('orderByRaw', 'ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(start_time)) ASC')
-          .fetch()
-          .then(function(model) {
-            eventId = model.get('id');
-            eventStartTime = moment(model.get('start_time'));
+    // Get the event_id of the closest event in time
+    .then(function() {
+      new models.Event()
+        .query('orderByRaw', 'ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(start_time)) ASC')
+        .fetch()
+        .then(function(model) {
+          eventId = model.get('id');
+          eventStartTime = moment(model.get('start_time'));
 
-          // Update the event_participant status and check-in time
-          }).then(function(model) {
-            status = (eventStartTime.format('X') - now.format('X') >= 0) ? 'ontime' : 'late';
-            new models.EventParticipant({event_id: eventId, participant_id: participantId})
-              .fetch()
-              .then(function(model) {
-                if (model && !model.get('status')) {
-                  // Record exists with a null status, update it
-                  model.set('status', status);
-                  model.set('checkin_time', moment().format('YYYY-MM-DD HH:mm:ss'));
-                  model.save();
-                } else if (!model) {
-                  // Record doesn't exist, create it
-                  models.EventParticipant.forge({
-                    event_id: eventId,
-                    participant_id: participantId,
-                    status: status,
-                    checkin_time: now.format('YYYY-MM-DD HH:mm:ss')
-                  }).save();
-                } else {
-                  // Status is already set, do nothing
-                  return;
-                }
-                callback({
-                  deviceId: deviceId,
-                  eventId: eventId,
-                  status: status
-                });
+        // Update the event_participant status and check-in time
+        }).then(function(model) {
+          status = (eventStartTime.format('X') - now.format('X') >= 0) ? 'ontime' : 'late';
+          new models.EventParticipant({event_id: eventId, participant_id: participantId})
+            .fetch()
+            .then(function(model) {
+              if (model && !model.get('status')) {
+                // Record exists with a null status, update it
+                model.set('status', status);
+                model.set('checkin_time', moment().format('YYYY-MM-DD HH:mm:ss'));
+                model.save();
+              } else if (!model) {
+                // Record doesn't exist, create it
+                models.EventParticipant.forge({
+                  event_id: eventId,
+                  participant_id: participantId,
+                  status: status,
+                  checkin_time: now.format('YYYY-MM-DD HH:mm:ss')
+                }).save();
+              } else {
+                // Status is already set, do nothing
+                return;
+              }
+              callback({
+                deviceId: deviceId,
+                eventId: eventId,
+                status: status
               });
-          });
-      })
+            });
+        });
+    })
 
-      .catch(function(error) {
-        console.error('An error occured checking in the user', error);
-      });
-  }
+    .catch(function(error) {
+      console.error('An error occured checking in the user', error);
+    });
+};
 
+module.exports = {
+  checkinUser: checkinUser,
+  getEventParticipants: getEventParticipants,
+  getCurrentEvent: getCurrentEvent,
+  getCheckinStatus: getCheckinStatus,
+  getParticipant: getParticipant,
+  getEvents: getEvents,
+  updateDeviceId: updateDeviceId
 };
--- server/models.js
+++ server/models.js
@@ -2,85 +2,97 @@ var bookshelf = require('./db/db');
 
 // Define bookshelf models
 
-module.exports = exports = {
-  Admin: bookshelf.Model.extend({
-    tableName: 'admins',
-    events: function() {
-      return this.hasMany(Event);
-    }
-  }),
+var Admin = bookshelf.Model.extend({
+  tableName: 'admins',
+  events: function() {
+    return this.hasMany(Event);
+  }
+});
 
-  Participant: bookshelf.Model.extend({
-    tableName: 'participants',
-    events: function() {
-      return this.belongsToMany(Event).through(EventParticipant);
-    },
-    status: function() {
-      return this.hasMany(EventParticipant);
-    }
-  }),
+var Participant = bookshelf.Model.extend({
+  tableName: 'participants',
+  events: function() {
+    return this.belongsToMany(Event).through(EventParticipant);
+  },
+  status: function() {
+    return this.hasMany(EventParticipant);
+  }
+});
 
-  Event: bookshelf.Model.extend({
-    tableName: 'events',
-    participants: function() {
-      return this.belongsToMany(Participant)
-        .through(EventParticipant)
-        .withPivot(['status', 'checkin_time']);
-    },
-    admin: function() {
-      return this.belongsTo(Admin);
-    },
-    status: function() {
-      return this.hasMany(EventParticipant);
-    }
-  }),
+var Event = bookshelf.Model.extend({
+  tableName: 'events',
+  participants: function() {
+    return this.belongsToMany(Participant)
+      .through(EventParticipant)
+      .withPivot(['status', 'checkin_time']);
+  },
+  admin: function() {
+    return this.belongsTo(Admin);
+  },
+  status: function() {
+    return this.hasMany(EventParticipant);
+  }
+});
 
-  EventParticipant: bookshelf.Model.extend({
-    tableName: 'events_participants',
-    participant: function() {
-      return this.belongsTo(Participant);
-    },
-    event: function() {
-      return this.belongsTo(Event);
-    }
-  }),
+var EventParticipant = bookshelf.Model.extend({
+  tableName: 'events_participants',
+  participant: function() {
+    return this.belongsTo(Participant);
+  },
+  event: function() {
+    return this.belongsTo(Event);
+  }
+});
 
-  Beacon: bookshelf.Model.extend({
-    tableName: 'beacons',
-    events: function() {
-      return this.belongsToMany(Event).through(beacons_events);
-    }
-  }),
+var Beacon = bookshelf.Model.extend({
+  tableName: 'beacons',
+  events: function() {
+    return this.belongsToMany(Event).through(beacons_events);
+  }
+});
 
-  BeaconEvent: bookshelf.Model.extend({
-    tableName: 'beacons_events',
-    beacon: function() {
-      return this.belongsTo(Beacon);
-    },
-    event: function() {
-      return this.belongsTo(Event);
-    }
-  })
-};
+var BeaconEvent = bookshelf.Model.extend({
+  tableName: 'beacons_events',
+  beacon: function() {
+    return this.belongsTo(Beacon);
+  },
+  event: function() {
+    return this.belongsTo(Event);
+  }
+});
 
 // Define bookshelf collections
 
-exports.EventsParticipants = bookshelf.Collection.extend({
-  model: exports.EventParticipant
+var EventsParticipants = bookshelf.Collection.extend({
+  model: EventParticipant
 });
 
-exports.Events = bookshelf.Collection.extend({
-  model: exports.Event
+var Events = bookshelf.Collection.extend({
+  model: Event
 });
 
-exports.Beacons = bookshelf.Collection.extend({
-  model: exports.Beacon
+var Beacons = bookshelf.Collection.extend({
+  model: Beacon
 });
 
-exports.BeaconsEvents = bookshelf.Collection.extend({
-  model: exports.BeaconEvent
+var BeaconsEvents = bookshelf.Collection.extend({
+  model: BeaconEvent
 });
 
-exports.Participants = bookshelf.Collection.extend({
-  model: exports.Participant
+var Participants = bookshelf.Collection.extend({
+  model: Participant
 });
+
+module.exports = {
+  Admin: Admin,
+  Participant: Participant,
+  Participants: Participants,
+  Event: Event,
+  Events: Events,
+  EventParticipant: EventParticipant,
+  EventsParticipants: EventsParticipants,
+  Beacon: Beacon,
+  Beacons: Beacons,
+  BeaconsEvent: BeaconEvent,
+  BeaconsEvents: BeaconsEvents
+};
