3ea5cf68c575206ad79a0c0843cac79fe7959550
Sebastian Tonkin <sgtonkin@gmail.com>
2015-01-06 14:54:36 -0800

    Fixed gulp errors

--- test/server/serverspec.js
+++ test/server/serverspec.js
@@ -1,12 +1,13 @@
 var expect = require('chai').expect;
 var request = require('request');
 
-describe('REST API - GET', function(){
+describe('REST API - GET', function() {
 
   it('Can get the beacons for a device id', function(done) {
     var deviceId = 999;
+    var url = 'http://localhost:8080/api/devices/' + deviceId + '/beacons';
 
-    request('http://localhost:8080/api/devices/'+deviceId+'/beacons', function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
@@ -14,8 +15,9 @@ describe('REST API - GET', function(){
 
   it('Can get the beacons for a device id', function(done) {
     var participantId = 1;
+    var url = 'http://localhost:8080/api/participants/' + participantId + '/events';
 
-    request('http://localhost:8080/api/participants/'+participantId+'/events', function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
@@ -23,17 +25,19 @@ describe('REST API - GET', function(){
 
   it('Can get event participants for a given eventId', function(done) {
     var eventId = 1;
+    var url = 'http://localhost:8080/api/participants/' + eventId + '/events';
 
-    request('http://localhost:8080/api/participants/'+eventId+'/events', function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
   });
 
-  it('Can get the event info for any events happening within 1 hour of now for a given participant', function(done) {
+  it('Can get the current event for a participant', function(done) {
     var eventId = 1;
+    var url = 'http://localhost:8080/api/participants/' + eventId + '/events/current';
 
-    request('http://localhost:8080/api/participants/'+eventId+'/events/current', function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(body.match(/Error/g)).to.equal(null);
       done();
     });
@@ -41,8 +45,8 @@ describe('REST API - GET', function(){
 
   it('Can get all events for a given admin ID', function(done) {
     var adminId = 1;
-
-    request('http://localhost:8080/api/admins/'+adminId+'/events', function(error, res, body) {
+    var url = 'http://localhost:8080/api/admins/' + adminId + '/events';
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
@@ -50,8 +54,9 @@ describe('REST API - GET', function(){
 
   it('Can get the admin name for a given admin ID', function(done) {
     var adminId = 1;
+    var url = 'http://localhost:8080/api/admins/' + adminId;
 
-    request('http://localhost:8080/api/admins/'+adminId, function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
@@ -59,8 +64,9 @@ describe('REST API - GET', function(){
 
   it('Can get the participant info for a given device ID', function(done) {
     var deviceId = 999;
+    var url = 'http://localhost:8080/api/devices/' + deviceId + '/participant';
 
-    request('http://localhost:8080/api/devices/'+deviceId+'/participant', function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
@@ -69,8 +75,9 @@ describe('REST API - GET', function(){
   it('Can get the checkin status for a given device and event', function(done) {
     var deviceId = 999;
     var eventId = 1;
+    var url = 'http://localhost:8080/api/devices/' + deviceId + '/events/' + eventId + '/status';
 
-    request('http://localhost:8080/api/devices/'+deviceId+'/events/'+eventId+'/status', function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
@@ -78,14 +85,12 @@ describe('REST API - GET', function(){
 
   it('Can get all the beacons for a given admin', function(done) {
     var adminId = 1;
+    var url = 'http://localhost:8080/api/admins/' + adminId + '/beacons';
 
-    request('http://localhost:8080/api/admins/'+adminId+'/beacons', function(error, res, body) {
+    request(url, function(error, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
   });
 
-
-
 });
-
