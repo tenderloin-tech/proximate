05fb6102eb093d7bf62827bb9708d2f84c6f1a3d
Sebastian Tonkin <sgtonkin@gmail.com>
2015-01-06 11:52:17 -0800

    Added test and request packages, created empty server spec

---
+++ test/server/serverspec.js
@@ -0,0 +1,18 @@
+var expect = require('chai').expect;
+
+var xbeforeEach = function(){};
+
+describe('REST API', function(){
+
+  beforeEach(function(done){
+
+    done();
+
+  });
+
+  it('Can do something', function(done) {
+      expect(1).to.equal(404);
+  });
+
+});
+
