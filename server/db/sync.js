554a815e0d1e018ae867e5e636d9fa4a892b39b9
Sebastian Tonkin <sgtonkin@gmail.com>
2015-01-06 18:54:51 -0800

    Added underscore, added function to fetch calendars

--- server/db/sync.js
+++ server/db/sync.js
@@ -2,16 +2,25 @@ var promise = require('bluebird');
 var models = require('../models');
 var auth = require('../auth');
 var helpers = require('./helpers');
+var _ = require('underscore');
 
 exports.sync = function() {
-  helpers.getAdminTokens('sgtonkin@gmail.com')
-    .then(function(tokens) {
-      console.log('tokens', tokens);
-      auth.setCredentials(tokens);
 
-      var calendar = require('googleapis').calendar({version: 'v3', auth: auth});
+  var calendar = require('googleapis').calendar({version: 'v3', auth: auth});
+  var calendarIds = [];
 
-      var query = {
+  var getCalendars = function() {
+    var params = {
+      minAccessRole: 'owner'
+    }
+
+    calendar.calendarList.list(params, function(err, data) {
+      calendarIds = _.pluck(data.items, 'id');
+      console.log(calendarIds);
+    });
+  }
+
+  var query = {
         calendarId: 'sgtonkin@gmail.com',
         maxResults: 2500,
         orderBy: 'updated',
@@ -19,10 +28,15 @@ exports.sync = function() {
         singleEvents: true
       }
 
-      calendar.events.list(query, function(err, data) {
-        console.log('data', data);
-        console.log('err', err);
-      });
+
+  helpers.getAdminTokens('sgtonkin@gmail.com')
+    .then(function(tokens) {
+      auth.setCredentials(tokens);
+      getCalendars();
+      // calendar.events.list(query, function(err, data) {
+      //   console.log('data', data);
+      //   console.log('err', err);
+      // });
     })
 
 
