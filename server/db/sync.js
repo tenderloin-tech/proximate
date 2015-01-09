786c4c031e18f66225a721f6e8f1450d02cacc54
Sebastian Tonkin <sgtonkin@gmail.com>
2015-01-08 19:45:57 -0800

    Inserting participants and data prepped for events_participants helper

--- server/db/sync.js
+++ server/db/sync.js
@@ -108,37 +108,113 @@ exports.sync = function() {
         return true;
       })
       .map(function(attendee) {
-        return {
+        return [{
           email: attendee.email,
           name: attendee.displayName || null
-        }
+        },{
+          gcal_response_status: attendee.responseStatus
+        }]
       })
       .value();
   }
 
+  // Format event_participant info an map to db fields
+
+  var formatStatus = function(eventId, attendeeInfo) {
+    // console.log('eventId', eventId);
+    // console.log('attendeeInfo', attendeeInfo);
+    // participantIds = _.chain(attendeeInfo)
+    // .map(function(attendee) {
+    //   return formatStatus(attendee.attributes, ;
+    // })
+    // .uniq()
+    // .value();
+  }
+
   // Pull out the event info
   var updateDb = function(events) {
-    // Format participants for insertion
-    var participants = _.map(events, function(event) {
-      return formatAttendees(event.attendees);
-    })
-
-
     // Format events for insertion and insert them
+
+    // Accept events that have attendees
+      // These attendee records have a response status for a given event
+      // We want to update the event in question and get an id for the event db record
+      // Next we want to update any participant records in the db
+      // Finally, once those participant records come back with an ID we want to combine them...
+        // back again with their response status for this event and write
+
     _.each(events, function(event) {
       var formattedEvent = formatEvents(event);
-      helpers.upsertEvent(formattedEvent);
+      var formattedAttendees = formatAttendees(event.attendees);
+      var eventId;
+      var participantIds;
+      var formattedStatus;
+
+      helpers.upsertEvent(formattedEvent)
+      .then(function(eventInfo) {
+        eventId = eventInfo.attributes.id;
+
+        // console.log('eventId', eventId);
+        // console.log('formatted attendees', formattedAttendees)
+
+        return promise.all(
+          _.map(formattedAttendees, function(attendee) {
+            console.log('attendee', attendee);
+            return helpers.upsertParticipant(attendee[0]);
+        }));
+      })
+      .then(function(attendeeInfo) {
+
+        // console.log('attendee info after db', attendeeInfo, "\n");
+
+        // Loop through each formatted attendee
+        // Find a record in attendeee info where the emails match
+        // Once you find that, add a new value to it that matches what we want to insert including eventId and participant id
+        // insert it
+
+        events_participants = _.map(formattedAttendees, function(formattedAttendee) {
+          var matchedRecord = _.find(attendeeInfo, function(attendee) {
+            // console.log('attendee', attendee, '\n');
+            // console.log('formatted email', formattedAttendee[0].email);
+            // console.log('attendee email', attendee.attributes.email);
+            return formattedAttendee[0].email === attendee.attributes.email;
+          });
+
+          return {
+            event_id: eventId,
+            participantId: matchedRecord.attributes.id,
+            gcal_response_status: formattedAttendee[1].gcal_response_status
+          }
+
+          // console.log('matchedRecord', matchedRecord);
+          // console.log('formattedAttendee', formattedAttendee, '\n');
+
+
+        })
+
+        console.log('events participants', events_participants);
+
+
+
+        // Combine the id field of this array with formattedAttendees based on a match on email
+
+        // var formattedStatus = formatStatus(eventId, attendeeInfo);
+
+        // For each participant ID, create or update an events_participants record
+
+
+      });
     });
 
     // Clear duplicates from participants and insert them
-    _.chain(participants)
-      .flatten()
-      .uniq(false, function(participant) {
-        return participant.email;
-      })
-      .each(function(participant) {
-        helpers.upsertParticipant(participant);
-      });
+    // _.chain(participants)
+    //   .flatten()
+    //   .uniq(false, function(participant) {
+    //     return participant.email;
+    //   })
+    //   .each(function(participant) {
+    //     helpers.upsertParticipant(participant);
+    //   });
+
 
 
   }
@@ -162,7 +238,7 @@ exports.sync = function() {
           return isValid(event);
         })
         .value()
-      // Update the events in the db
+      // Take our filtered list of events and insert into db
       return updateDb(events);
     })
     .catch(function(error) {
@@ -173,3 +249,49 @@ exports.sync = function() {
 
 
 exports.sync();
+
+
+// Before insert
+
+// formatted attendees [ [ { email: 'sgtonkin@gmail.com', name: 'Sebastian Tonkin' },
+//     { gcal_response_status: 'needsAction' } ],
+//   [ { email: 'avidunn@gmail.com', name: 'Avi Dunn' },
+//     { gcal_response_status: 'needsAction' } ],
+//   [ { email: 'poison5151@gmail.com', name: 'Valentyn Boginskey' },
+//     { gcal_response_status: 'needsAction' } ],
+//   [ { email: 'derek.barncard@gmail.com',
+//       name: 'Derek Ryan Barncard' },
+//     { gcal_response_status: 'accepted' } ] ]
+
+// After db
+
+// { attributes:
+// { email: 'sgtonkin@gmail.com',
+//        id: 68,
+//        name: 'Sebastian Tonkin',
+//        device_id: null },
+//     _previousAttributes:
+//      { email: 'sgtonkin@gmail.com',
+//        id: 68,
+//        name: 'Sebastian Tonkin',
+//        device_id: null },
+//     changed: {},
+//     relations: {},
+//     cid: 'c116',
+//     _knex: null,
+//     id: 68 },
+//   { attributes:
+//      { email: 'avidunn@gmail.com',
+//        id: 39,
+//        name: 'Avi Dunn',
+//        device_id: null },
+//     _previousAttributes:
+//      { email: 'avidunn@gmail.com',
+//        id: 39,
+//        name: 'Avi Dunn',
+//        device_id: null },
+//     changed: {},
+//     relations: {},
+//     cid: 'c117',
+//     _knex: null,
+//     id: 39 },
