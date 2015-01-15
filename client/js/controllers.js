4cb471a402826d967bf05fe9833816ec12ddfec5
Derek Barncard <derek@barncard.com>
2015-01-14 19:20:49 -0800

    Stash for rebase

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -126,6 +126,15 @@ angular.module('proximate.controllers', [])
 
 .controller('EventsCtrl', function($scope, $state, Populate) {
 
+  $scope.displayFilter = 'past';
+
+  $scope.setDisplayFilter = function(time) {
+    console.log(time)
+    if (time){
+      $scope.displayFilter = time;
+    }
+  };
+
   // Click handler for getting roster for a single event
   $scope.getEventRoster = function(event, eventId) {
     event.preventDefault();
