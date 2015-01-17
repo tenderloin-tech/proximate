087778f3ffa04aca115fafe094c76ccc29a2b491
Sebastian Tonkin <sgtonkin@gmail.com>
2015-01-17 10:34:38 -0800

    Fixed gulp errors

--- server/models.js
+++ server/models.js
@@ -13,7 +13,8 @@ var Admin = exports.Admin = bookshelf.Model.extend({
   currentEvent: function() {
     return this.hasMany(Event)
     .query(function(qb) {
-      return qb.whereRaw('ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(events.start_time)) <= 86400 AND (events.status != "cancelled")')
+      return qb.whereRaw('ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(events.start_time)) <=' +
+        '86400 AND (events.status != "cancelled")')
         .orderByRaw('ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(events.start_time)) ASC')
         .limit(1);
     });
@@ -32,7 +33,8 @@ var Participant = exports.Participant = bookshelf.Model.extend({
     return this.belongsToMany(Event)
     .through(EventParticipant)
     .query(function(qb) {
-      return qb.whereRaw('ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(events.start_time)) <= 3600 AND (events.status != "cancelled")')
+      return qb.whereRaw('ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(events.start_time)) <=' +
+        '3600 AND (events.status != "cancelled")')
         .orderByRaw('ABS(UNIX_TIMESTAMP() - UNIX_TIMESTAMP(events.start_time)) ASC')
         .limit(1);
     });
